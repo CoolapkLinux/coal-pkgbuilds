75c3cf74d104cb98386431398cc4e653908ed1dd
Oganesson <59897809+oganesson0512@users.noreply.github.com>
2020-11-14 13:27:39 +0000

    Create update.sh

---
+++ pkgbuild/atilo/update.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+#Update atilo's PKGBUILD
+pkgname=atilo
+pkgver=$(cat pkgbuild/${pkgname}/PKGBUILD|grep pkgver=|awk -F\= '{print $2}')
+pkgonlinever=$(curl https://api.github.com/repos/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $4}')/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $5}')/releases/latest|grep tag_name|awk -F\" '{print $4}')
+if [ ${pkgver} != ${pkgonlinever} ]; then
+sed -i '/pkgver=/cpkgver='"${pkgonlinever}" pkgbuild/${pkgname}/PKGBUILD
+echo "$pkg" >> need-update
+fi
