7ea49b1f66b4fc681874260111b973ac2203a626
Oganesson <59897809+oganesson0512@users.noreply.github.com>
2020-11-14 20:56:23 +0800

    Update update.sh

--- pkgbuild/purewriter/update.sh
+++ pkgbuild/purewriter/update.sh
@@ -2,7 +2,6 @@
 pkgname=purewriter
 pkgver=$(cat pkgbuild/${pkgname}/PKGBUILD|grep pkgver=|awk -F\= '{print $2}')
 pkgonlinever=$(curl https://api.github.com/repos/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $4}')/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $5}')/releases/latest|grep tag_name|awk -F\" '{print $4}')
-echo "$pkgver" "$pkgonlinever"
 if [ ${pkgver} != ${pkgonlinever} ]; then
 sed -i '/pkgver=/cpkgver='"${pkgonlinever}" pkgbuild/${pkgname}/PKGBUILD
 echo "$pkg" >> need-update
