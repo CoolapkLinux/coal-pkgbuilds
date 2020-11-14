8490fd2dd89c51489bd0718f9ad844552f3a8b61
Oganesson <59897809+oganesson0512@users.noreply.github.com>
2020-11-14 14:08:14 +0000

    Update and rename update-gh-release.sh to update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,14 @@
 #/bin/bash
 #Update package using github api;suit for github release
 pkgname=$1
+arg=$2
 pkgver=$(cat pkgbuild/${pkgname}/PKGBUILD|grep pkgver=|awk -F\= '{print $2}')
 pkgonlinever=$(curl https://api.github.com/repos/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $4}')/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $5}')/releases/latest|grep tag_name|awk -F\" '{print $4}')
+case arg in
+v)
+  pkgonlinever="v${pkgonlinever}"
+::
+esac
 if [ ${pkgver} != ${pkgonlinever} ]; then
 	sed -i '/pkgver=/cpkgver='"${pkgonlinever}" pkgbuild/${pkgname}/PKGBUILD
 	echo "${pkgname}" >> need-update
