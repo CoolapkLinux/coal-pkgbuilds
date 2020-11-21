7a3e18d16009e586c2da763a509da3f4bbc27937
Oganesson <59897809+oganesson0512@users.noreply.github.com>
2020-11-21 20:11:39 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,23 @@
 #/bin/bash
 #Update package using github api;suit for github release
-pkgname=$1
-arg=$2
-pkgver=$(cat pkgbuild/${pkgname}/PKGBUILD|grep pkgver=|awk -F\= '{print $2}')
-pkgonlinever=$(curl https://api.github.com/repos/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $4}')/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $5}')/releases/latest|grep tag_name|awk -F\" '{print $4}')
-case arg in
-v)
-  pkgonlinever=$(echo ${pkgonlinever}|sed 's/v//g')
-::
-esac
-if [ ${pkgver} != ${pkgonlinever} ]; then
-	sed -i '/pkgver=/cpkgver='"${pkgonlinever}" pkgbuild/${pkgname}/PKGBUILD
-	echo "${pkgname}" >> need-update
-fi
+update(){
+  pkgname=$1
+  arg=$2
+  pkgver=$(cat pkgbuild/${pkgname}/PKGBUILD|grep pkgver=|awk -F\= '{print $2}')
+  pkgonlinever=$(curl https://api.github.com/repos/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $4}')/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $5}')/releases/latest|grep tag_name|awk -F\" '{print $4}')
+  case arg in
+  v)
+    pkgonlinever=$(echo ${pkgonlinever}|sed 's/v//g')
+  ::
+  esac
+  if [ ${pkgver} != ${pkgonlinever} ]; then
+    sed -i '/pkgver=/cpkgver='"${pkgonlinever}" pkgbuild/${pkgname}/PKGBUILD
+    echo "${pkgname}" >> need-update
+  fi
+  return 0
+}
+for pkgname in `ls -AF pkgbuild |grep '/$' | tr -d '/'`
+do
+  examine_arg=(cat pkgbuild/${pkgname}/PKGBUILD|grep \#examine=|awk -F\= '{print $2}')
+  update(${pkgname} ${examine_arg})
+done
