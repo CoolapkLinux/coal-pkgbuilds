4959741143c811f767503033464211240372e873
oganesson0512 <w1301511091@hotmail.com>
2021-02-18 16:29:21 +0800

    Change a lot

--- pkgbuild/atilo/update.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-#Update atilo's PKGBUILD
-pkgname=atilo
-pkgver=$(cat ./PKGBUILD|grep pkgver=|awk -F\= '{print $2}')
-pkgonlinever=$(curl https://api.github.com/repos/$(cat ./PKGBUILD|grep source=|awk -F\/ '{print $4}')/$(cat ./PKGBUILD|grep source=|awk -F\/ '{print $5}')/releases/latest|grep tag_name|awk -F\" '{print $4}')
-if [ ${pkgver} != ${pkgonlinever} ]; then
-sed -i '/pkgver=/cpkgver='"${pkgonlinever}" ./PKGBUILD
-echo "$pkg" >> need-update
-fi
--- pkgbuild/purewriter/update.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-#Update Pure Writer's PKGBUILD
-pkgname=purewriter
-pkgver=$(cat pkgbuild/${pkgname}/PKGBUILD|grep pkgver=|awk -F\= '{print $2}')
-pkgonlinever=$(curl https://api.github.com/repos/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $4}')/$(cat pkgbuild/${pkgname}/PKGBUILD|grep source=|awk -F\/ '{print $5}')/releases/latest|grep tag_name|awk -F\" '{print $4}')
-if [ ${pkgver} != ${pkgonlinever} ]; then
-sed -i '/pkgver=/cpkgver='"${pkgonlinever}" pkgbuild/${pkgname}/PKGBUILD
-echo "$pkg" >> need-update
-fi
